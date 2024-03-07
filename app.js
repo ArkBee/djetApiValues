bfd970ae5ef53c4569ad9fbb6e68a2e5ac4601f7
ArkBee <93475033+ArkBee@users.noreply.github.com>
2024-03-07 20:50:07 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,3 +2,9 @@ document.getElementById('randomNumberButton').addEventListener('click', function
     var randomNumber = Math.floor(Math.random() * 100) + 1;
     document.getElementById('randomNumberDisplay').innerText = "Рандомное число: " + randomNumber;
 });
+
+window.addEventListener('storage', (event) => {
+  if (event.key === 'myData') {
+    console.log('Полученные данные: ', event.newValue);
+  }
+});
