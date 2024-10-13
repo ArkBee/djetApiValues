685b53bb19658e7da10a239f7e898bfacffe3ec2
Thomas Kessler <93475033+ArkBee@users.noreply.github.com>
2024-10-13 18:44:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -32,6 +32,7 @@ function checkAccelerometer() {
                 if (lastX !== undefined) {
                     const diff = Math.abs(x - lastX) + Math.abs(y - lastY) + Math.abs(z - lastZ);
                     if (diff > 0.1) changes++;  // Небольшое изменение
+                    document.body.innerHTML += '<h1>СДВИГ ЕСТЬ</h1>';
                 }
                 lastX = x;
                 lastY = y;
@@ -40,6 +41,7 @@ function checkAccelerometer() {
 
             window.addEventListener('devicemotion', motionHandler);
 
+            document.body.innerHTML = '<h1>ЧЕКАЕМ АКСЕЛЬ</h1>';
             // Проверяем через 3 секунды
             setTimeout(() => {
                 window.removeEventListener('devicemotion', motionHandler);
